e153fa02ef37cdc8edddb20db5d218b203c8f22c
Christopher Koch <koch.c@mail.de>
2016-11-30 00:17:01 +0100

    added simple css style + json

--- v0.0.3/WebClient_0.0.3/client.js
+++ v0.0.3/WebClient_0.0.3/client.js
@@ -3,14 +3,14 @@ var clientID = -1;
 
 $(document).ready(function(){
 	// connect to server
-	server = new WebSocket("ws://192.168.178.21:81");
+	server = new WebSocket("ws://192.168.178.47:81");
 	
 	server.onopen = function (event) {
 		var msg = {
 			type: "message",
 			text: "This is Text!",
 			id:   clientID,
-			timestamp: Date.now()
+			timestamp: new Date().getTime()/1000
 		};
 		server.send( JSON.stringify( msg ) ); 
 	};
@@ -18,4 +18,14 @@ $(document).ready(function(){
 	server.onmessage = function (event) {
 		console.log(event.data);
 	}
+	
+	$('#test1').on('change', function(){
+		var msg = {
+			type: "LED1",
+			text: $(this).is(':checked'),
+			id:   clientID,
+			timestamp: new Date().getTime()/1000
+		};
+		server.send( JSON.stringify( msg ) ); 
+	});
 });
